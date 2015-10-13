8e53c874613c828bf4a23c108f736bbe9057acba
Rubens <rubens001@gmail.com>
2015-10-13 10:34:22 -0300

    info

--- nwshell/app.js
+++ nwshell/app.js
@@ -9,11 +9,13 @@ path = process.env.RESOURCE || __dirname + '/public';
 console.log('port=' + port + ' resource='+path);
 
 app.use(express.static(path));
-app.use(bodyParser.json()); // parse application/json so "body" became an object
+// parse application/json so "request.body" became an object
+app.use(bodyParser.json());
 app.listen(port);
 
 app.post('/nwshell',function(req, res) {
 	// console.log('post em nwshell, url=', req.url);
+	// console.log('post em nwshell, scmd.cmd=', req.body.cmd);
 	console.log('post em nwshell, scmd.cmd=', req.body.cmd);
 	res.json({ret:'ok',time:(new Date())});
 });
