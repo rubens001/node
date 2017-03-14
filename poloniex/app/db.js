598a7335af989e03277f4d70c5e5a62ea01c82cb
Rubens <rubens001@gmail.com>
2017-03-14 09:04:02 -0300

    inMemory

--- poloniex/app/db.js
+++ poloniex/app/db.js
@@ -1,8 +1,13 @@
 // lowdb dbjson
 const config = require('../app/config').get(); 
 const low = require('lowdb');
-const storage = require('lowdb/lib/storages/file-sync'); // file-async ou file-sync
-const db = config.inMemoryLow ? low() : low(config.dbPath, { storage });
+var db;
+if (config.inMemoryLow) {
+	db = low();
+} else {
+	const storage = require('lowdb/lib/storages/file-async'); // file-async ou file-sync
+	db = low(config.dbPath, { storage });
+}
 
 exports.get=function() {
 	// Set some defaults if your JSON file is empty
