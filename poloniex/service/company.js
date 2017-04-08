4115aa96ae10ffc5bff82d8e00e5071b0675e964
Rubens <rubens001@gmail.com>
2017-04-08 14:37:34 -0300

    retirar projeto

--- poloniex/service/company.js
+++
@@ -1,7 +0,0 @@
-exports.get=function(req,res,next) {
-  // evita 304 para servicos
-  req.headers['if-none-match'] = 'no-match-for-this';
-
-	var co = {name:"7chaos",address:"Paulista, 123"};
-	res.json(co);
-};
